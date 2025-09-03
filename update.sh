ec95e71c397bcf91aa9c191cde64f00f5205ec05
nerdguy-lab <joshuamutiso814@gmail.com>
2025-09-03 16:50:41 +0300

    Added update.sh file

---
+++ update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+release_file=/etc/os-release
+
+if grep -q "Arch" $release_file
+then
+    # The host is based on Arch, run the pacman update command
+    sudo pacman -Syu
+fi
+
+if grep -q "Ubuntu" $release_file || grep -q "Debian" $release_file
+then
+    # The host is based on Debian or Ubuntu,
+    # Run the apt version of the command
+    sudo apt update
+    sudo apt dist-upgrade
+fi
