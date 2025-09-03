12414486c6cd4ff2c3fff0410647451e3041a0eb
nerdguy-lab <joshuamutiso814@gmail.com>
2025-09-03 22:26:59 +0300

    Added data-streams.sh file

---
+++ data-streams.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+release_file=/etc/os-release
+logfile=/logfiles/updater.log
+errorlog=/logfiles/updater_errors.log
+
+if grep -q "Arch" $release_file
+then
+    # The host is based on Arch, run the pacman update command
+    sudo pacman -Syu 1>>$logfile 2>>$errorlog
+    if [ $? -ne 0 ]
+    then
+        echo "An error occured, please check the $errorlog file."
+    fi    
+fi
+
+if grep -q "Ubuntu" $release_file || grep -q "Debian" $release_file
+then
+    # The host is based on Debian or Ubuntu,
+    # Run the apt version of the command
+    sudo apt update 1>>$logfile 2>>$errorlog
+    if [ $? -ne 0 ]
+    then
+        echo "An error occured, please check the $errorlog file."
+    fi 
+    sudo apt dist-upgrade 1>>$logfile 2>>$errorlog
+    if [ $? -ne 0 ]
+    then
+        echo "An error occured, please check the $errorlog file."
+    fi 
+fi
